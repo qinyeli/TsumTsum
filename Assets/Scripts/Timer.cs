5eae2e26db94ccef5bb900fd81ab22398fd72d89
Qinye Li <qinyeli@umich.edu>
2017-10-19 21:37:14 -0700

    Count Down System completed (Timer.cs)

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * Timer counts down 1 min. If it finds a UI Text element named TimeGUI, it updates
+ * the TimeGUI text, but it works fine without TimeGUI.
+ */
+public class Timer : MonoBehaviour {
+	
+	Text timeText;
+	float time = 60;
+
+	void Start() {
+		GameObject canvas = GameObject.Find ("Canvas");
+		if (canvas != null) {
+			Transform timeGUI = canvas.transform.Find ("TimeGUI");
+			if (timeGUI != null) {
+				timeText = timeGUI.GetComponent<Text> ();
+				SyncTimeGUI ();
+			}
+		}
+	}
+
+	void Update() {
+		time -= Time.deltaTime;
+		if (time < 0) {
+			time = 0;
+		}
+		SyncTimeGUI ();
+	}
+
+	void SyncTimeGUI() {
+		if (timeText != null) {
+			timeText.text = ((int)time).ToString ();
+		}
+	}
+}
